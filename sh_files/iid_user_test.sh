578ca267431f07d8fa6289518236e46c0e7742c7
rkdgus0 <create0327@naver.com>
2024-05-08 15:40:18 +0900

    [05/08] sj 수정

--- sh_files/iid_user_test.sh
+++ sh_files/iid_user_test.sh
@@ -1,22 +1,21 @@
 cd ../
-num_cpu=4
 
 gpu_id=0
 group_name='Gang_Test'
-exp_name='Resnet50(pt)_lr0.002_iid_u10'
-  python3 main.py -n_round 50 -n_users 50 -n_epoch 2 -eval_round 1 \
+exp_name='Resnet50(pt)_lr0.002_iid_u50'
+  python3 main.py -n_rounds 100 -n_users 50 -n_epochs 2 -eval_round 1 \
   -model resnet50 -pre_trained -opt sgd -lr 0.002 -lr_decay 0.99 -lr_decay_round 5 \
-  -dataset mnist -batch_size 32 -split iid \
-  -wandb True -group_name $group_name -exp_name $exp_name \
+  -dataset cifar10 -batch_size 32 -split iid \
+  -wandb -group_name $group_name -exp_name $exp_name \
   -gpu_id $gpu_id
 
 gpu_id=3
 group_name='Gang_Test'
-exp_name='Resnet50(pt)_lr0.002_iid_u20'
-  python3 main.py -n_round 50 -n_users 100 -n_epoch 2 -eval_round 1 \
+exp_name='Resnet50(pt)_lr0.002_iid_u100'
+  python3 main.py -n_rounds 100 -n_users 100 -n_epochs 2 -eval_round 1 \
   -model resnet50 -pre_trained -opt sgd -lr 0.002 -lr_decay 0.99 -lr_decay_round 5 \
-  -dataset mnist -batch_size 32 -split iid \
-  -wandb True -group_name $group_name -exp_name $exp_name \
+  -dataset cifar10 -batch_size 32 -split iid \
+  -wandb -group_name $group_name -exp_name $exp_name \
   -gpu_id $gpu_id
 
 wait
