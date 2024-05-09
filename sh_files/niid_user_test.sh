7f2185ae13b287f7ccc684ee3ea32dcaad43704b
rkdgus0 <create0327@naver.com>
2024-05-09 20:11:44 +0900

    asdf

--- sh_files/niid_user_test.sh
+++ sh_files/niid_user_test.sh
@@ -1,13 +1,23 @@
 cd ../
 num_cpu=10
 
+gpu_id=0
 group_name='Gang_Test'
-exp_name='Resnet50(pt)_lr0.002_niid_u10'
-taskset -cpu-list $((gpu_id*num_cpu))-$((gpu_id*num_cpu+num_cpu-1)) \
-  python3 main.py -n_round 50 -n_users 10 -n_epoch 2 -eval_round 1 \
-  -model resnet50 -pre_trained -opt sgd -lr 0.002 -lr_decay 0.99 -lr_decay_round 5 \
-  -dataset mnist -batch_size 32 -split non_iid \
-  -wandb True -group_name $group_name -exp_name $exp_name \
+exp_name='resnet50_lr0.075_niid_u50_epoch2'
+  python3 main.py -n_rounds 100 -n_users 50 -n_epochs 2 -eval_round 1 \
+  -model resnet50 -opt sgd -lr 0.003 -lr_decay 0.99 -lr_decay_round 5 \
+  -dataset cifar10 -batch_size 32 -split non_iid \
+  -wandb -group_name $group_name -exp_name $exp_name \
+  -gpu_id $gpu_id &
+
+gpu_id=1
+group_name='Gang_Test'
+exp_name='resnet50_lr0.075_niid_u100_epoch2'
+  python3 main.py -n_rounds 100 -n_users 100 -n_epochs 2 -eval_round 1 \
+  -model resnet50 -opt sgd -lr 0.003 -lr_decay 0.99 -lr_decay_round 5 \
+  -dataset cifar10 -batch_size 32 -split non_iid \
+  -wandb -group_name $group_name -exp_name $exp_name \
+  -gpu_id $gpu_id &
 
 wait
 echo "Experiments completed!"
