0cfe3b323049f591e4e61d192f9ac5687f4b197e
rkdgus0 <create0327@naver.com>
2024-05-09 19:12:50 +0900

    [05/09] add mean graph

--- sh_files/iid_user_test.sh
+++ sh_files/iid_user_test.sh
@@ -1,22 +1,18 @@
 cd ../
 
-gpu_id=0
-group_name='Gang_Test'
-exp_name='mcmahan2NN_lr0.075_iid_u10_epoch2'
+group_name1='Gang_Test'
+exp_name1='mcmahan2NN_lr0.075_iid_u10_epoch2'
   python3 main.py -n_rounds 50 -n_users 10 -n_epochs 2 -eval_round 1 \
   -model mcmahan2NN -opt sgd -lr 0.075 -lr_decay 0.99 -lr_decay_round 5 \
-  -dataset cifar10 -batch_size 32 -split iid \
-  -wandb -group_name $group_name -exp_name $exp_name \
-  -gpu_id $gpu_id &
+  -dataset mnist -batch_size 32 -split iid \
+  -wandb -group_name $group_name1 -exp_name $exp_name1 &
 
-gpu_id=1
-group_name='Gang_Test'
-exp_name='mcmahan2NN_lr0.075_iid_u50_epoch2'
+group_name2='Gang_Test'
+exp_name2='mcmahan2NN_lr0.075_iid_u50_epoch2'
   python3 main.py -n_rounds 50 -n_users 50 -n_epochs 2 -eval_round 1 \
   -model mcmahan2NN -opt sgd -lr 0.075 -lr_decay 0.99 -lr_decay_round 5 \
-  -dataset cifar10 -batch_size 32 -split iid \
-  -wandb -group_name $group_name -exp_name $exp_name \
-  -gpu_id $gpu_id &
+  -dataset mnist -batch_size 32 -split iid \
+  -wandb -group_name $group_name2 -exp_name $exp_name2 &
 
 wait
 echo "Experiments completed!"
